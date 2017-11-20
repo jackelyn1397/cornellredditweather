b920844f9357a90f4b8c767a2e1d617ee5f57cfe
Jackelyn Shen <admin@Georges-MacBook-Air-2.local>
2017-11-20 00:55:31 -0500

    year round weather data and broken text analytics

---
+++ text_analytics.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import httplib, urllib
+import json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the accessKey string value with your valid access key.
+accessKey = '09aed0988aac4d2586f3bcd9d7322991'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your access keys.
+# For example, if you obtained your access keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+# a free trial access key, you should not need to change this region.
+uri = 'westus.api.cognitive.microsoft.com'
+path_sentiment = '/text/analytics/v2.0/sentiment'
+path_keyphrases = '/text/analytics/v2.0/keyPhrases'
+
+def GetSentiment (documents):
+    "Gets the sentiments for a set of documents and returns the information."
+
+    headers = {'Ocp-Apim-Subscription-Key': accessKey}
+    conn = httplib.HTTPSConnection (uri)
+    body = json.dumps (documents)
+    conn.request ("POST", path_sentiment, body, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+def GetKeyPhrases (documents):
+    "Gets the sentiments for a set of documents and returns the information."
+
+    headers = {'Ocp-Apim-Subscription-Key': accessKey}
+    conn = httplib.HTTPSConnection (uri)
+    body = json.dumps (documents)
+    conn.request ("POST", path_keyphrases, body, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+
+documents = { 'documents': [
+    { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id': '2', 'language': 'es', 'text': 'Si usted quiere comunicarse con Carlos, usted debe de llamarlo a su telefono movil. Carlos es muy responsable, pero necesita recibir una notificacion si hay algun problema.' },
+    { 'id': '3', 'language': 'en', 'text': 'The Grand Hotel is a new hotel in the center of Seattle. It earned 5 stars in my review, and has the classiest decor I\'ve ever seen.' }
+]}
+
+print 'Please wait a moment for the results to appear.\n'
+
+result_sentiment = GetSentiment (documents)
+result_keyphrases = GetKeyPhrases (documents)
+
+print (json.dumps(json.loads(result_sentiment), indent=4))
+print (json.dumps(json.loads(result_keyphrases), indent=4))
+
+
--- weather.py
+++ weather.py
@@ -1,5 +1,50 @@
 import urllib2
 import json
+year = 2017
+maxDays = 0
+for m in range(1, 13):
+	if(m==2):
+		if(year % 4==0):
+			maxDays = 30
+		else:
+			maxDays = 29
+	elif(m==1 or m==3 or m==5 or m==7 or m==8 or m==10 or m==12):
+		maxDays = 32
+	else:
+		maxDays = 31
+	for d in range(1, maxDays):
+		api_date = str(year)
+		if(m<10):
+			api_date+="0"+str(m)
+		else:
+			api_date+=str(m)
+		if(d<10):
+			api_date+="0"+str(d)
+		else:
+			api_date+=str(d)
+		f = urllib2.urlopen('http://api.wunderground.com/api/ee7193fad42bdca9/history_'+api_date+'/q/NY/Ithaca.json')
+		json_string = f.read()
+		parsed_json = json.loads(json_string)
+		date = parsed_json['history']['date']['pretty']
+		temp = parsed_json['history']['dailysummary'][0]['meantempi']
+		numHours = len(parsed_json['history']['observations'])
+		dictionary = {}
+		for i in range(numHours):
+			cond = parsed_json['history']['observations'][i]['conds']
+			if cond in dictionary:
+				dictionary[cond] = dictionary[cond]+1
+			else:
+				dictionary[cond] = 1
+		num = 0
+		condition = ""
+		for key, value in dictionary.items():
+			if value > num:
+				num = value
+				condition = key
+		print "Date: %s, Condition: %s, Temperature: %s" % (date, condition, temp)
+		f.close()
+
+"""
 f = urllib2.urlopen('http://api.wunderground.com/api/ee7193fad42bdca9/history_20170103/q/NY/Ithaca.json')
 json_string = f.read()
 parsed_json = json.loads(json_string)
@@ -21,3 +66,4 @@ for key, value in dictionary.items():
 		condition = key
 print "Date: %s, Condition: %s, Temperature: %s" % (date, condition, temp)
 f.close()
+"""
